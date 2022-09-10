47656bb4fa824b56be995b65156ce7a976683425
YasminBlancoRuiz <yazminblanco1906@gmail.com>
2022-09-09 22:06:57 -0500

    Se agrega  typeDocumento

---
+++ api/serializers/typeDocuSerializer.py
@@ -0,0 +1,22 @@
+from dataclasses import fields
+from api.models.typeDocument import TypeDocument
+from rest_framework import serializers
+
+#serializer es para transformar de JSON a una cuenta
+class typeDocumentSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = TypeDocument
+    fields = ['id', 'code', 'name']
+
+
+    def create(self, validated_data): 
+        typeDocumentInstace=TypeDocument.objects.create(**validated_data)
+        return typeDocumentInstace
+
+    def to_representation(self, obj): 
+        typeDocument=TypeDocument.objects.get(id=obj.id)
+        return{
+            'id':typeDocument.id,
+            'code':typeDocument.code,
+            'name':typeDocument.name
+        }
