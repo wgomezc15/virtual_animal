a31d071544054ac5d796ff9ae8e7bc3882dfb9fc
Yulany Munevar <andra_852@hotmail.com>
2022-09-11 23:38:29 -0500

    agregando urls, e importanto clases a views

--- animalProject/urls.py
+++ animalProject/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('user/', views.UserCreateView.as_view()),
     path('user/<int:pk>/', views.UserDetailView.as_view()),
     path('typedocument/', views.TypeDocumentCreateView.as_view()),
+    path('typedocument/<int:pk>/', views.TypeDocumentGetView.as_view()),
+    path('typedocumentall/', views.TypeDocumentGetAllView.as_view()),
+    path('typedocumentadelete/<int:id>/', views.TypeDocumentDeleteView.as_view()),
+    path('typedocumentupdate/', views.TypeDocumentUpdateView.as_view()),
 ]
