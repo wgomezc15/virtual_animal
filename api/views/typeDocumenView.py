7d9e024243a74083ba5a66f33db989f766b0ec95
william-gomez <wgomezc15@gmail.com>
2022-09-11 23:10:13 -0500

    Update init

--- api/views/typeDocumenView.py
+++ api/views/typeDocumenView.py
@@ -5,35 +5,10 @@ from rest_framework.response import Response
 from api.models.typeDocument import TypeDocument
 from api.serializers.typeDocumentSerializer import TypeDocumentSerializer
 
-class TypeDocumentGetView(generics.RetrieveAPIView):
-    queryset = TypeDocument.objects.all()
-    serializer_class = TypeDocumentSerializer
-    
-    def get(self, request, *args, **kwargs):
-        return super().get(request, *args, **kwargs)
-
 class TypeDocumentCreateView(views.APIView):
 
     def post(self, request):
         serializer = TypeDocumentSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()               
-        return Response(None, status=status.HTTP_200_OK)
-
-class TypeDocumentGetAllView(generics.RetrieveAPIView):
-
-    def get(self,request):
-        obj = list(TypeDocument.objects.values())     
-        return JsonResponse(obj, safe=False) 
-
-class TypeDocumentDeleteView(views.APIView):
-
-    def get(self, request, id):
-        TypeDocument.objects.filter(id=id).delete()            
-        return Response(None, status=status.HTTP_200_OK)
-
-class TypeDocumentUpdateView(views.APIView):
-
-    def post(self, request):
-        TypeDocument.objects.filter(id=request.data["id"]).update(code=request.data["code"], name = request.data["name"])
         return Response(None, status=status.HTTP_200_OK)
