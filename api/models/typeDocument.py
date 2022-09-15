add0bab21ef8d1096993a90d8160179b50efc2e6
william-gomez <wgomezc15@gmail.com>
2022-09-14 22:12:14 -0500

    Se realiza mejoras para implementar GET, POS, PUT Y DELETE para typeDocument

--- api/models/typeDocument.py
+++ api/models/typeDocument.py
@@ -4,3 +4,5 @@ class TypeDocument(models.Model):
     id = models.AutoField(primary_key=True)
     code = models.CharField('Code',max_length=4)
     name = models.CharField('Name',max_length=20)
+    class Meta:
+        ordering = ['id']
