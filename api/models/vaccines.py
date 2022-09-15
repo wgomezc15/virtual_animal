ba3091c7bb8c936aef9b3c798a41238880250b39
Yulany Munevar <andra_852@hotmail.com>
2022-09-14 23:02:47 -0500

    Creación del modelo vacunas

---
+++ api/models/vaccines.py
@@ -0,0 +1,8 @@
+from django.db import models
+from .pet import Pet
+
+class AnimalVaccinations(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField('Name', max_length=60)
+    idPet=models.ForeignKey(Pet, related_name='petID',on_delete=models.PROTECT)
+
