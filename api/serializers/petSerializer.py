32512790dc4289dce64bc97f5071ac7a0a8beedc
AdrianaGaviria <adrigavi0502@gmail.com>
2022-09-19 06:48:51 -0500

    Se crea CRUD para mascota

--- api/serializers/petSerializer.py
+++ api/serializers/petSerializer.py
@@ -1,28 +1,7 @@
-from dataclasses import fields
-from api.models.pet import Pet
 from rest_framework import serializers
+from api.models import Pet
 
-#serializer es para transformar de JSON a una cuenta
 class PetSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Pet
-    fields = ['id', 'name','age','race','sex','species','features','isActive','userClient']
-
-
-    def create(self, validated_data): 
-        petInstace=Pet.objects.create(**validated_data)
-        return petInstace
-
-    def to_representation(self, obj): 
-        pet=Pet.objects.get(id=obj.id)
-        return{
-            'id':pet.id,
-            'name':pet.name,
-            'age':pet.age,
-            'race':pet.race,
-            'sex':pet.sex,
-            'species':pet.species,
-            'features':pet.features,
-            'isActive':pet.isActive,
-            'userClient':pet.userClient
-        }
+    class Meta:
+        model = Pet
+        fields = ['id','name','age','race','sex','species','features','isActive','userClient']
