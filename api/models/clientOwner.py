3c2d1b363d25129df485feee94997a53dded1e45
william-gomez <wgomezc15@gmail.com>
2022-09-25 20:56:20 -0500

    Ajustes en clientOwner

--- api/models/clientOwner.py
+++ api/models/clientOwner.py
@@ -6,7 +6,7 @@ class ClientOwner(models.Model):
     lastname = models.CharField('Lastname',max_length=30)
     email = models.EmailField('Email',max_length=100)
     document = models.CharField('Document',max_length= 15,unique=True)
-    typedocument = models.ForeignKey(TypeDocument,related_name='typedocclientowner',on_delete=models.PROTECT)
+    typedocument = models.ForeignKey(TypeDocument,related_name='clientowner',on_delete=models.PROTECT)
     telephone = models.CharField('Telephone',max_length=20)
     address = models.CharField('Address',max_length=100)
     gender = models.CharField('Gender',max_length=1)
